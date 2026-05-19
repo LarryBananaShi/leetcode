3ca7f9909c364e923a173dd1f33a42133bd8ca57
LarryBananaShi <161532782+LarryBananaShi@users.noreply.github.com>
2026-05-18 21:39:49 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        count = {} #track letter count (key) to values (strs) #
+        #edit: can use count = defaultdict(list) to automatically assign missing values, if block can be removed
+        for word in strs:
+            array = [0]*26 # array of 26 0s
+
+            for char in word:
+                array[ord(char)- ord("a")] +=1
+            
+            if tuple(array) in count:
+                count[tuple(array)].append(word)
+            else:
+                count[tuple(array)] = [word]
+
+
+
+        return list(count.values()) #.values() doesn't return list; have to wrap it in 'list' if desired output is list            
+            
+            #for each string, make an array that has all 26 letters and holds count for each letter
+            #if array in hashmap (use array as key), store value of current str into array at that key value
+
+
+
+        #first solution
+        #for every single string in the string list, we need to
+        #hashmap with a key value pair where the key is a letter and the 
+        #value is the frequency of that letter. add each of these hash maps
+        #to a new list
+
+        #this is one for loop ^
+
+        #now that we have all the hash maps in the list, we iterate through that list
+        #and group similar pairs
