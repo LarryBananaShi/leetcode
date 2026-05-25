c1a69cee96786be3b97c50e344794e65da3a16cb
Larry Shi <161532782+LarryBananaShi@users.noreply.github.com>
2026-05-25 18:06:35 -0400

    Add: validate-parentheses - submission-13

---
+++ Data Structures & Algorithms/validate-parentheses/submission-13.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        bracketMap = {")":"(", "}":"{", "]":"["}
+
+        #finish later, better solution is to uese a hashmap for 
+        #the opening/closing brackets
+        stack = []
+        for c in s:
+            if c in bracketMap:
+                if stack:
+                    top = stack.pop()
+                else:
+                    top = "placeholder"
+                print(top)
+                print(bracketMap[c])
+                if (bracketMap[c]!=top):
+                    return False
+            else:
+                stack.append(c)
+            
+                
+        if not stack:
+            return True
+        else:
+            return False
+            
+            
+
+
+
+
+        
