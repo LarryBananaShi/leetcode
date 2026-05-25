e899681381d0feaf8ed333ceec94f70904da9b21
Larry Shi <161532782+LarryBananaShi@users.noreply.github.com>
2026-05-25 19:55:54 -0400

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums)-1
+        while(l <= r):
+            midpoint = (l+r)//2
+            if (nums[midpoint]<target):
+                l = midpoint+1
+            elif (nums[midpoint]>target):
+                r = midpoint-1
+            else:
+                return midpoint
+        return -1
+
+
+
+        #check middle value of array, bigger or smaller than 
+        #target. if smaller, check middle of the smaller half,
+        #vice versa, until you get to target
+
